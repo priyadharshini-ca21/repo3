b9ca19faf5f6c25fd1d3104c21095ca5530d4008
priyadharshini-ca21 <priyadharshinir.ca21@kascsathy.ac.in>
2023-07-31 09:52:20 +0000

    first commit

---
+++ sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()  {
+
+     int x,y,sum;
+
+     printf("Enter values of two numbers:");
+     scanf("%d %d",&x,&y);
+
+     printf("x=%d\n",x);
+     printf("y=%d\n",y);
+
+     sum=x+y;
+     printf("x+y=%d\n",sum);
+
+     return 0;
+}
